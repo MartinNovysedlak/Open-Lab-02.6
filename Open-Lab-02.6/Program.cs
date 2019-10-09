5df85335de8eb185a8973160b28c979b4f98f654
MartinNovysedlak <martinnovysedlak48@gmail.com>
2019-10-09 21:51:19 +0200

    Initial

---
+++ Open-Lab-02.6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        public static bool five(int number)
+        {
+            return number % 100 == 0;
+        }
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(five(number));
+        }
+    }
+}
